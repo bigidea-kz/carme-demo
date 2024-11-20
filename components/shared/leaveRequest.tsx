bfa5c7a9e93b616c82511379f64eb80c6d93816a
damir <kamerdino@gmail.com>
2024-11-20 15:18:43 +0500

    first draft:update leave request section

--- components/shared/leaveRequest.tsx
+++ components/shared/leaveRequest.tsx
@@ -5,11 +5,11 @@ export const LeaveRequest = () => {
         <div className="py-36">
             <div className="container mx-auto px-4 lg:px-0">
                 <div className="flex mx-auto p-16 rounded-[50px] bg-orange-50 gap-8">
-                    <div className="flex-1 space-y-2">
+                    <div className="flex-1 flex flex-col items-start space-y-4">
                         <h2 className="text-3xl font-semibold w-3/4">Мечтаете о новом авто, но кредит или полная оплата не подходят?</h2>
                         <div className="cursor-pointer flex items-center text-blue-600 text-md font-medium group">
-                            <span>оставить заявку</span>
-                            <ChevronRight className="ml-2 group-hover:scale-150 transition duration-300"/>
+                            <span>Оставить заявку</span>
+                            <ChevronRight className="ml-2 group-hover:translate-x-1 transition duration-300"/>
                         </div>
                     </div>
 
